243686faa7dc6112d5e9c347a66ac0f3c1ce83fc
Jwonik <122337370+Jwonik@users.noreply.github.com>
2023-03-11 15:15:30 +0900

    lv.1 - 서울에서 김서방 찾기

---
+++ CodingTest_Programmers/src/programmers/Solution13.java
@@ -0,0 +1,20 @@
+package programmers;
+
+// 서울에서 김서방 찾기
+public class Solution13 {
+	public String solution(String[] seoul) {
+        String answer = "";
+        int x = 0;
+        
+        for(int i = 0; i < seoul.length; i++) {
+        	if(seoul[i].equals("Kim")) {
+        		x = i;
+        		break;
+        	}
+        }
+        
+        answer = "김서방은 " + x + "에 있다";
+        
+        return answer;
+    }
+}
