19d5ba178483c3f35c3796e31910633ebd9bfe31
Jwonik <122337370+Jwonik@users.noreply.github.com>
2023-04-27 15:13:47 +0900

    lv.1 - 예산

---
+++ CodingTest_Programmers/src/programmers/Solution34.java
@@ -0,0 +1,23 @@
+package programmers;
+
+import java.util.Arrays;
+
+// 예산
+public class Solution34 {
+	public int solution(int[] d, int budget) {
+	      int answer = 0;
+
+	        Arrays.sort(d);
+
+	        for (int i = 0; i < d.length; i++) {
+	            budget -= d[i];
+
+	            if (budget < 0) break;
+
+	            answer++;
+	        }
+
+	        return answer;
+	  }
+}
+
